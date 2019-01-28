f4e43c740b4e26e3944ea72449c11dd2c44d2dc5
David Stanley <davearthur.stanley@gmail.com>
2019-01-27 22:31:46 -0500

    app/utils.py Added new code for saving post-processed data

--- app/utils.py
+++ app/utils.py
@@ -4,14 +4,14 @@ import numpy as np
 import json
 import os
 
-def load_SQuAD_data(filename,verbose=False):
+def load_SQuAD_data(filename,SQuAD_foldername='SQuAD',verbose=False):
     import json
     import os
 
     # Set up path info
     repodir = os.path.join(os.getenv("HOME"),'src','animated-succotash')
     datadir = os.path.join(repodir,'data')
-    squaddir = os.path.join(datadir,'SQuAD')
+    squaddir = os.path.join(datadir,SQuAD_foldername)
 
     fullname = os.path.join(squaddir,filename)
     if verbose: print(fullname)
@@ -39,10 +39,51 @@ def load_SQuAD_data(filename,verbose=False):
 
     return arts
 
+def save_SQuAD_data(arts,filename,SQuAD_foldername='SQuAD_postprocessed',verbose=False):
+    import json
+    import os
 
-def load_SQuAD_train(verbose=False):
-    return load_SQuAD_data('train-v2.0.json',verbose)
+    # Set up path info
+    repodir = os.path.join(os.getenv("HOME"),'src','animated-succotash')
+    datadir = os.path.join(repodir,'data')
+    squaddir = os.path.join(datadir,SQuAD_foldername)
+
+    # Create directory if doesn't already exist
+    if not os.path.exists(squaddir):
+        print("Directory " + squaddir + " doesn't exist. Creating..")
+        os.makedirs(squaddir)
+
+    fullname = os.path.join(squaddir,filename)
+    if verbose: print(fullname)
+
+    data = {'data': arts, 'version':['v2.0']}
+
+    # Save the data
+    with open(fullname, 'w') as outfile:
+        json.dump(data, outfile)
 
 
-def load_SQuAD_dev(verbose=False):
-    return load_SQuAD_data('dev-v2.0.json',verbose)
+# # # # # Loading data # # # # #
+# Raw data
+def load_SQuAD_train(filename='train-v2.0.json',verbose=False):
+    return load_SQuAD_data(filename,'SQuAD',verbose)
+
+def load_SQuAD_dev(filename='dev-v2.0.json',verbose=False):
+    return load_SQuAD_data(filename,'SQuAD',verbose)
+
+# Postprocessed data
+def load_SQuAD_train_pp(filename='train-v2.0.json',verbose=False):
+    # Load data from post processing folder
+    return load_SQuAD_data(filename,'SQuAD_postprocessed',verbose)
+
+def load_SQuAD_dev_pp(filename='dev-v2.0.json',verbose=False):
+    # Load data from post processing folder
+    return load_SQuAD_data(filename,'SQuAD_postprocessed',verbose)
+
+
+# # # # # Saving data # # # # #
+def save_SQuAD_train_pp(arts,filename='train-v2.0.json',verbose=False):
+    save_SQuAD_data(arts,filename,'SQuAD_postprocessed',verbose)
+
+def save_SQuAD_dev_pp(arts,filename='dev-v2.0.json',verbose=False):
+    save_SQuAD_data(arts,filename,'SQuAD_postprocessed',verbose)
