680092008643e5f638bef61cb38ff1aaebed643f
David Stanley <davearthur.stanley@gmail.com>
2019-02-02 15:51:24 -0500

    Created app/utils_EDAplots.py - Functionalizes plotting commands used throughout EDA

---
+++ app/utils_EDAplots.py
@@ -0,0 +1,32 @@
+
+
+
+def plotbar_train_dev(myvar,Ntrain,Ndev,varname,xlabel='Article #'):
+    # Import fig stuff
+    import matplotlib.pyplot as plt
+    from matplotlib.pyplot import figure
+
+    figure(num=None, figsize=(15, 4),facecolor='w', edgecolor='k')
+    barlist = plt.bar(range(len(myvar)), myvar, align='center', alpha=0.5)
+    plt.xlabel(xlabel)
+    plt.ylabel('{} per article'.format(varname))
+    for i in range(Ntrain,Ntrain+Ndev):
+        barlist[i].set_color('r')
+    plt.show()
+
+
+def plothist_train_dev(myvar,Ntrain,Ndev,varname,ylabel='N Articles'):
+    # Import fig stuff
+    import matplotlib.pyplot as plt
+    from matplotlib.pyplot import figure
+    import statistics
+
+    f, (ax1, ax2) = plt.subplots(1, 2, sharey=False,figsize=(15, 4));
+    ax1.hist(myvar[0:Ntrain-1], bins=30);  # arguments are passed to np.histogram
+    ax1.set_title("Narticles={}, median={}".format(str(Ntrain),str(statistics.median(myvar[0:Ntrain-1]))));
+    ax1.set_ylabel('N Articles');
+    ax1.set_xlabel('{}'.format(varname));
+
+    ax2.hist(myvar[Ntrain:], bins=30);  # arguments are passed to np.histogram
+    ax2.set_title("Narticles={}, median={}".format(str(Ndev),str(statistics.median(myvar[Ntrain:]))));
+    ax2.set_xlabel('{}'.format(varname));
