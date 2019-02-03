cd189f28218d407c5ae9a3b2a2b333437d903661
David Stanley <davearthur.stanley@gmail.com>
2019-02-02 22:43:37 -0500

    app/utils_EDA.py - Added function a_sentences_per_article

--- app/utils_EDA.py
+++ app/utils_EDA.py
@@ -8,3 +8,14 @@ def p_list_qas(paragraphs):
             print('Question ' + str(j) + ': ' + qa['question'])
             for k,a in enumerate(qa['answers']):
                 print('\tAnswer ' + str(k) + ': ' + a['text'])
+
+def a_sentences_per_article(art):
+    # Number of sentences per article
+    from nltk.tokenize import sent_tokenize
+
+    asentences = []
+    for a in art:
+        psentences = [len(sent_tokenize(p['context'])) for p in a['paragraphs']]
+        asentences.append(sum(psentences))
+
+    return asentences
