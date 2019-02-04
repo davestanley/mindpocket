70a3b49e7b6be96af08aa1ac806802e391a7cd36
David Stanley <davearthur.stanley@gmail.com>
2019-02-03 22:00:02 -0500

    Updated sh files to match prev commit

--- train_model/articles_batch4/model0b2/step2_train_model.sh
+++ train_model/articles_batch4/model0b2/step2_train_model.sh
@@ -1,1 +1,1 @@
-allennlp train ./simple_tagger_mod2_GLoVE.json --serialization-dir ./model_params
+allennlp train ./simple_tagger_mod.json --serialization-dir ./model_params
--- train_model/articles_batch4/model1b3/step2_train_model.sh
+++ train_model/articles_batch4/model1b3/step2_train_model.sh
@@ -1,1 +1,1 @@
-allennlp train ./crf_tagger_mod.json --serialization-dir ./model_params
+allennlp train ./simple_tagger_mod.json --serialization-dir ./model_params
