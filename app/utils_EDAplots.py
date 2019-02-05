ecbdb7f9b372793191c0d1115591ce7baa877147
David Stanley <davearthur.stanley@gmail.com>
2019-02-04 19:51:35 -0500

    app/utils_EDAplots.py - Modified plotting cmd

--- app/utils_EDAplots.py
+++ app/utils_EDAplots.py
@@ -23,10 +23,10 @@ def plothist_train_dev(myvar,Ntrain,Ndev,varname,ylabel='N Articles'):
 
     f, (ax1, ax2) = plt.subplots(1, 2, sharey=False,figsize=(15, 4));
     ax1.hist(myvar[0:Ntrain-1], bins=30);  # arguments are passed to np.histogram
-    ax1.set_title("Narticles={}, median={}".format(str(Ntrain),str(statistics.median(myvar[0:Ntrain-1]))));
+    ax1.set_title("Narticles={}, median={}, mean={}".format(str(Ntrain),'{0:.1f}'.format(statistics.median(myvar[0:Ntrain-1])),'{0:.1f}'.format(statistics.mean(myvar[0:Ntrain-1]))));
     ax1.set_ylabel('N Articles');
     ax1.set_xlabel('{}'.format(varname));
 
     ax2.hist(myvar[Ntrain:], bins=30);  # arguments are passed to np.histogram
-    ax2.set_title("Narticles={}, median={}".format(str(Ndev),str(statistics.median(myvar[Ntrain:]))));
+    ax2.set_title("Narticles={}, median={}, mean={}".format(str(Ndev),'{0:.1f}'.format(statistics.median(myvar[Ntrain:])),'{0:.1f}'.format(statistics.mean(myvar[Ntrain:]))));
     ax2.set_xlabel('{}'.format(varname));
