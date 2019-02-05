6f995f07909171c129546f328fbdfb4c54ab8b35
David Stanley <davearthur.stanley@gmail.com>
2019-02-04 23:17:48 -0500

    app/utils_allenNLP.py - Added prepend_data_folder option

--- app/utils_allenNLP.py
+++ app/utils_allenNLP.py
@@ -1,16 +1,19 @@
 # Utilities for running and testing AllenNLP code
 
-def run_predictor(art,predictor,foldername,filename_prefix,testing_mode=False,skip_save=False):
+def run_predictor(art,predictor,foldername,filename_prefix,testing_mode=False,skip_save=False,prepend_data_folder=True):
+    # prepend_data_folder - Adds ~/src/mindpocket/data prefix to squaddir folder name
     from utils import save_data, load_data, exists_datafolder
     from utils import merge_artfiles
+
+    verbose2_on=False
 
     # Loop through and add results field to data
     art2 = art.copy()
     for i,a in enumerate(art):
         filename = filename_prefix + '_art_' + str(i).zfill(3) + '.json'
-        
+
         # Do a short test to see if file exists
-        file_exists = exists_datafolder(filename,foldername)
+        file_exists = exists_datafolder(filename,foldername,prepend_data_folder)
         if file_exists:
             print("File: " + filename + " already exists. Skipping...")
             continue        # If file already exists, skip over to the next file
@@ -36,7 +39,7 @@ def run_predictor(art,predictor,foldername,filename_prefix,testing_mode=False,sk
             art2[i]['paragraphs'][j]['allenNER']=results2
 
         # Save individual articles
-        if not skip_save: save_data(art2[i],foldername)
+        if not skip_save: save_data(art2[i],filename,foldername,[],[],prepend_data_folder)
 
     # Once all individual files have been saved, merge into 1 large json file
-    merge_artfiles(filename_prefix + '_art_*',foldername,filename_prefix + '.json',verbose=True)
+    merge_artfiles(filename_prefix + '_art_*',foldername,filename_prefix + '.json',verbose=True,do_overwrite=[],prepend_data_folder=prepend_data_folder)
