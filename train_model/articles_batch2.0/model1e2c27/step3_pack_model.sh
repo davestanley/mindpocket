18d9e570815377d84a6d2e6a8d119b9650639ead
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 18:49:59 -0500

    Redid all symbolic links

--- train_model/articles_batch2.0/model1e2c27/step3_pack_model.sh
+++ train_model/articles_batch2.0/model1e2c27/step3_pack_model.sh
@@ -1,1 +1,16 @@
-../../step3_pack_model.sh
+
+# Make folder and copy necessary files over
+mkdir model
+cp model_params/best.th model/weights.th
+cp model_params/config.json model/
+cp -r model_params/vocabulary model/
+
+# Zip the model (make sure just zipping the files, not the folder)
+cd model
+tar -cvzf  model.tar.gz *
+cd ..
+
+# Copy zip file back here and delete the directory
+cp model/model.tar.gz .
+rm -rf model
+
