8c1975af81bb386e71076d4737fe03a7feb5b49d
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 18:12:58 -0500

    Added new files for linking to model dir

---
+++ train_model/link_model_files.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Change to model directory and run this command when
+# running a new modeling experiment
+ln -s ../run_model_allarticles.py run_model_allarticles.py
+ln -s ../../step2_train_model.sh step2_train_model.sh
+ln -s ../../step3_pack_model.sh step3_pack_model.sh
+
