bf91dc1143ee2345122c026746e8a5188b188000
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 09:56:40 -0500

    Added articles_batch2.0/model1e2c27

---
+++ train_model/articles_batch2.0/model1e2c27/myallennlp/dataset_readers/sequence_tagging2.py
@@ -0,0 +1,224 @@
+# Modified version of sequence_tagging dataset reader to allow specifying a custom
+# tokenizer that operates on each sentence. Allows capturing pos tags, ner tags, and
+# dependencies. Using spacy tagger (see https://spacy.io/usage/spacy-101#annotations-pos-deps)
+
+
+
+from typing import Dict, List
+import logging
+
+from overrides import overrides
+
+from allennlp.common.file_utils import cached_path
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import TextField, SequenceLabelField, MetadataField, Field
+from allennlp.data.instance import Instance
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token
+    # davedit
+from allennlp.data.tokenizers import Tokenizer, WordTokenizer
+
+
+# Davedit - load spacey
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+DEFAULT_WORD_TAG_DELIMITER = "###"
+
+@DatasetReader.register("sequence_tagging2")
+class SequenceTaggingDatasetReader(DatasetReader):
+    """
+    Reads instances from a pretokenised file where each line is in the following format:
+
+    WORD###TAG [TAB] WORD###TAG [TAB] ..... \n
+
+    and converts it into a ``Dataset`` suitable for sequence tagging. You can also specify
+    alternative delimiters in the constructor.
+
+    Parameters
+    ----------
+    word_tag_delimiter: ``str``, optional (default=``"###"``)
+        The text that separates each WORD from its TAG.
+    token_delimiter: ``str``, optional (default=``None``)
+        The text that separates each WORD-TAG pair from the next pair. If ``None``
+        then the line will just be split on whitespace.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional (default=``{"tokens": SingleIdTokenIndexer()}``)
+        We use this to define the input representation for the text.  See :class:`TokenIndexer`.
+        Note that the `output` tags will always correspond to single token IDs based on how they
+        are pre-tokenised in the data file.
+    """
+    def __init__(self,
+                 word_tag_delimiter: str = DEFAULT_WORD_TAG_DELIMITER,
+                 token_delimiter: str = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 lazy: bool = False,
+                 mytokenizer: Tokenizer = None,           # davedit
+                 use_spacy_directly: bool = False) -> None:            #davedit
+        super().__init__(lazy)
+        self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+        self._word_tag_delimiter = word_tag_delimiter
+        self._token_delimiter = token_delimiter
+        self._mytokenizer = mytokenizer or WordTokenizer()
+        self._use_spacy_directly = use_spacy_directly
+
+    @overrides
+    def _read(self, file_path):
+        debug_mode = False
+        batch_process = True
+        import time
+
+        # if `file_path` is a URL, redirect to the cache
+        file_path = cached_path(file_path)
+
+        # Do full batch
+        if batch_process:
+            t0 = time.time()
+            with open(file_path, "r") as data_file:
+
+                logger.info("Reading instances from lines in file at: %s", file_path)
+                sentences_batch = []
+                tags_batch = []
+                for line in data_file:
+                    line = line.strip("\n")
+                    # skip blank lines
+                    if not line:
+                        continue
+
+                    tokens_and_tags = [pair.rsplit(self._word_tag_delimiter, 1)
+                                       for pair in line.split(self._token_delimiter)]
+
+                    # davedit
+                    # Get text only
+                    words = [word for word, tag in tokens_and_tags]
+                    sentences_batch.append(' '.join(words))
+
+                    tags_batch.append([tag for token, tag in tokens_and_tags])
+
+            # Batch process tokens (faster)
+            tokens_batch = self._mytokenizer.batch_tokenize(sentences_batch)
+
+            t1 = time.time()
+            total = t1-t0
+            print("Token batch process time:")
+            print(total)
+
+            for i in range(len(tokens_batch)):
+                tokens = tokens_batch[i]
+                tags = tags_batch[i]
+
+                # Bug checking because spacy is stupid....grrrr
+                if debug_mode:
+                    words = sentences_batch[i].split()
+                    words2 = [t.text for t in tokens]
+                    if not words == words2:
+                        import pdb
+                        pdb.set_trace()
+
+                # If these don't match up, it will produce an error. In this csae,
+                # pre-emptively enter debug mode
+                if not len(tokens) == len(tags):
+                    # This is the orignal way tokens would have been calculated in sequence_tagging.py
+                    tokens2 = [Token(text=token) for token, tag in tokens_and_tags]
+                    print("Error -misamatch between tokens and tags")
+                    print(tokens)
+                    print(tokens2)
+
+                    import pdb
+                    pdb.set_trace()
+
+                yield self.text_to_instance(tokens, tags)
+
+        if not batch_process:
+            t0 = time.time()
+            with open(file_path, "r") as data_file:
+
+                logger.info("Reading instances from lines in file at: %s", file_path)
+                for line in data_file:
+                    line = line.strip("\n")
+
+                    # skip blank lines
+                    if not line:
+                        continue
+
+
+                    tokens_and_tags = [pair.rsplit(self._word_tag_delimiter, 1)
+                                       for pair in line.split(self._token_delimiter)]
+
+                    # davedit
+                    # Get text only
+                    words = [word for word, tag in tokens_and_tags]
+                    sentence = ' '.join(words)
+
+                    if self._use_spacy_directly:
+                        # # # # Usings spacy directly # # # #
+                        # Load into spacy
+                        doc = nlp(sentence)
+
+                        # Calculate pos
+                        spacy_pos = [token.pos_ for token in doc]
+
+                        # Calculate tags
+                        spacy_tags = [token.tag_ for token in doc]
+
+                        # Calculate NER tags
+                        pos_tags = [token.ent_type_ for token in doc]
+
+                        # Testing only
+                        # temp = [(tt[0],tt[1],a,b) for tt,a,b in zip(tokens_and_tags,spacy_pos,spacy_tags)]
+                        tokens = [Token(text=tt[0],pos=pos,tag=tag) for tt,pos,tag in zip(tokens_and_tags,spacy_pos,spacy_tags)]
+                    else:
+                        # Use specified mytokenizer
+                        # Need to be sure to use modified white space tokenizer (registerd as whitespacy)
+                        # because the default option doesn't always reproduce the original tokenization
+                        # from versoin re-joined by spaces above.
+                        tokens = self._mytokenizer.tokenize(sentence)
+
+                    #tokens = [Token(text=token) for token, tag in tokens_and_tags]
+                    tags = [tag for token, tag in tokens_and_tags]
+
+                    # Bug checking because spacy is stupid....grrrr
+                    if debug_mode:
+                        words2 = [t.text for t in tokens]
+                        if not words == words2:
+                            import pdb
+                            pdb.set_trace()
+
+                    # If these don't match up, it will produce an error. In this csae,
+                    # pre-emptively enter debug mode
+                    if not len(tokens) == len(tags):
+                        # This is the orignal way tokens would have been calculated in sequence_tagging.py
+                        tokens2 = [Token(text=token) for token, tag in tokens_and_tags]
+                        print("Error -misamatch between tokens and tags")
+                        print(tokens)
+                        print(tokens2)
+
+                        import pdb
+                        pdb.set_trace()
+
+                    yield self.text_to_instance(tokens, tags)
+            t1 = time.time()
+            total = t1-t0
+            print("Iterated token processing time:")
+            print(total)
+
+    def text_to_instance(self, tokens: List[Token], tags: List[str] = None) -> Instance:  # type: ignore
+        """
+        We take `pre-tokenized` input here, because we don't have a tokenizer in this class.
+        """
+        # pylint: disable=arguments-differ
+        fields: Dict[str, Field] = {}
+        sequence = TextField(tokens, self._token_indexers)
+        fields["tokens"] = sequence
+        fields["metadata"] = MetadataField({"words": [x.text for x in tokens]})
+        # import code
+        # code.interact(local=locals())
+        # import pdb
+        # pdb.set_trace()
+        # print(len(sequence))
+        # print(len(tags))
+        # print(sequence)
+        if tags is not None:
+            fields["tags"] = SequenceLabelField(tags, sequence)
+        return Instance(fields)
---
+++ train_model/articles_batch2.0/model1e2c27/myallennlp/models/simple_tagger2.py
@@ -0,0 +1,204 @@
+# Simple tagger module with option to weight cross-entropy. For dealing with
+# class imbalance
+# Dave Stanley, Insight Data Science, 2019
+from typing import Dict, Optional, List, Any
+
+import numpy
+from overrides import overrides
+import torch
+from torch.nn.modules.linear import Linear
+import torch.nn.functional as F
+
+from allennlp.common.checks import check_dimensions_match
+from allennlp.data import Vocabulary
+from allennlp.modules import Seq2SeqEncoder, TimeDistributed, TextFieldEmbedder
+from allennlp.models.model import Model
+from allennlp.nn import InitializerApplicator, RegularizerApplicator
+from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
+from allennlp.training.metrics import CategoricalAccuracy
+
+
+@Model.register("simple_tagger2")
+class SimpleTagger2(Model):
+    """
+    This ``SimpleTagger`` simply encodes a sequence of text with a stacked ``Seq2SeqEncoder``, then
+    predicts a tag for each token in the sequence.
+
+    Parameters
+    ----------
+    vocab : ``Vocabulary``, required
+        A Vocabulary, required in order to compute sizes for input/output projections.
+    text_field_embedder : ``TextFieldEmbedder``, required
+        Used to embed the ``tokens`` ``TextField`` we get as input to the model.
+    encoder : ``Seq2SeqEncoder``
+        The encoder (with its own internal stacking) that we will use in between embedding tokens
+        and predicting output tags.
+    initializer : ``InitializerApplicator``, optional (default=``InitializerApplicator()``)
+        Used to initialize the model parameters.
+    regularizer : ``RegularizerApplicator``, optional (default=``None``)
+        If provided, will be used to calculate the regularization penalty during training.
+    """
+
+    def __init__(self, vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 encoder: Seq2SeqEncoder,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 do_crossentropy_weighting: bool = False,
+                 Ntags0: int = None,
+                 Ntags1: int = None,
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super(SimpleTagger2, self).__init__(vocab, regularizer)
+
+        self.text_field_embedder = text_field_embedder
+        self.num_classes = self.vocab.get_vocab_size("labels")
+        self.encoder = encoder
+        # Cross entropy weighting -davedit
+        self.do_crossentropy_weighting = do_crossentropy_weighting
+            # Should be integer (e.g., 0-100), but actual range doesn't matter becuase we will normalize later
+        self.Ntags0 = Ntags0            # Number of tags == 0 in whole dataset
+        self.Ntags1 = Ntags1            # Number of tags == 1 in whole dataset
+        self.tag_projection_layer = TimeDistributed(Linear(self.encoder.get_output_dim(),
+                                                           self.num_classes))
+
+        check_dimensions_match(text_field_embedder.get_output_dim(), encoder.get_input_dim(),
+                               "text field embedding dim", "encoder input dim")
+        self.metrics = {
+                "accuracy": CategoricalAccuracy(),
+                "accuracy3": CategoricalAccuracy(top_k=3)
+        }
+
+        initializer(self)
+
+    @overrides
+    def forward(self,  # type: ignore
+                tokens: Dict[str, torch.LongTensor],
+                tags: torch.LongTensor = None,
+                metadata: List[Dict[str, Any]] = None) -> Dict[str, torch.Tensor]:
+        # pylint: disable=arguments-differ
+        """
+        Parameters
+        ----------
+        tokens : Dict[str, torch.LongTensor], required
+            The output of ``TextField.as_array()``, which should typically be passed directly to a
+            ``TextFieldEmbedder``. This output is a dictionary mapping keys to ``TokenIndexer``
+            tensors.  At its most basic, using a ``SingleIdTokenIndexer`` this is: ``{"tokens":
+            Tensor(batch_size, num_tokens)}``. This dictionary will have the same keys as were used
+            for the ``TokenIndexers`` when you created the ``TextField`` representing your
+            sequence.  The dictionary is designed to be passed directly to a ``TextFieldEmbedder``,
+            which knows how to combine different word representations into a single vector per
+            token in your input.
+        tags : torch.LongTensor, optional (default = None)
+            A torch tensor representing the sequence of integer gold class labels of shape
+            ``(batch_size, num_tokens)``.
+        metadata : ``List[Dict[str, Any]]``, optional, (default = None)
+            metadata containing the original words in the sentence to be tagged under a 'words' key.
+
+        Returns
+        -------
+        An output dictionary consisting of:
+        logits : torch.FloatTensor
+            A tensor of shape ``(batch_size, num_tokens, tag_vocab_size)`` representing
+            unnormalised log probabilities of the tag classes.
+        class_probabilities : torch.FloatTensor
+            A tensor of shape ``(batch_size, num_tokens, tag_vocab_size)`` representing
+            a distribution of the tag classes per word.
+        loss : torch.FloatTensor, optional
+            A scalar loss to be optimised.
+
+        """
+
+
+
+
+        embedded_text_input = self.text_field_embedder(tokens)
+        batch_size, sequence_length, _ = embedded_text_input.size()
+        mask = get_text_field_mask(tokens)
+        encoded_text = self.encoder(embedded_text_input, mask)
+
+        logits = self.tag_projection_layer(encoded_text)
+        reshaped_log_probs = logits.view(-1, self.num_classes)
+        class_probabilities = F.softmax(reshaped_log_probs, dim=-1).view([batch_size,
+                                                                          sequence_length,
+                                                                          self.num_classes])
+
+        output_dict = {"logits": logits, "class_probabilities": class_probabilities}
+        # x = tokens['tokens']
+        # y = tokens['pos_tag']
+        # w = tokens['ner_tag']
+
+        # import code
+        # code.interact(local=locals())
+        # import pdb
+        # pdb.set_trace()
+
+
+        if tags is not None:
+            if self.do_crossentropy_weighting:
+                # Implementing custom loss function, weight tags = 1 vs tags = 0
+                # Note this only works for binary tags at present
+
+
+                Nt0 = self.Ntags0          # Should correspond to non-blanks
+                Nt1 = self.Ntags1             # Should correspond to blanks
+
+                if not (Nt0 and Nt1):
+                    # If either Nt0 or Nt1 are unspecified
+                    Nt0 = sum(sum((tags==0).double()))
+                    Nt1 = sum(sum((tags==1).double()))
+
+                # Convert N blanks to weights - weight is inversely proportional to number of tags
+                t0wp = Nt1 / (Nt0 + Nt1)*100      # t0 weighting percent
+                t1wp = Nt0 / (Nt0 + Nt1)*100       # t1 weighting percent
+                mask2 = mask                        # That was pointless....
+                mask2[tags == 1] = mask2[tags == 1] * t1wp
+                mask2[tags == 0] = mask2[tags == 0] * t0wp
+                loss = sequence_cross_entropy_with_logits(logits, tags, mask2)
+
+                # # Old code, hardcoded
+                # Nnonblanks = 5877.15 - 256.16     # Average number of non-blanks per article
+                # Nblanks = 256.16                  # Average # blanks per article
+                # blank_weight = Nnonblanks / (Nnonblanks + Nblanks)*100
+                # nblank_weight = Nblanks / (Nnonblanks + Nblanks)*100
+                # mask2 = mask
+                # mask2[tags == 1] = mask2[tags == 1] * blank_weight
+                # mask2[tags == 0] = mask2[tags == 0] * nblank_weight
+                # loss = sequence_cross_entropy_with_logits(logits, tags, mask2)
+            else:
+                # Defualt AllenNLP loss
+                loss = sequence_cross_entropy_with_logits(logits, tags, mask)
+
+            # import pdb
+            # pdb.set_trace()
+
+            for metric in self.metrics.values():
+                metric(logits, tags, mask.float())
+            output_dict["loss"] = loss
+
+        if metadata is not None:
+            output_dict["words"] = [x["words"] for x in metadata]
+        return output_dict
+
+    @overrides
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        Does a simple position-wise argmax over each token, converts indices to string labels, and
+        adds a ``"tags"`` key to the dictionary with the result.
+        """
+        all_predictions = output_dict['class_probabilities']
+        all_predictions = all_predictions.cpu().data.numpy()
+        if all_predictions.ndim == 3:
+            predictions_list = [all_predictions[i] for i in range(all_predictions.shape[0])]
+        else:
+            predictions_list = [all_predictions]
+        all_tags = []
+        for predictions in predictions_list:
+            argmax_indices = numpy.argmax(predictions, axis=-1)
+            tags = [self.vocab.get_token_from_index(x, namespace="labels")
+                    for x in argmax_indices]
+            all_tags.append(tags)
+        output_dict['tags'] = all_tags
+        return output_dict
+
+    @overrides
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {metric_name: metric.get_metric(reset) for metric_name, metric in self.metrics.items()}
