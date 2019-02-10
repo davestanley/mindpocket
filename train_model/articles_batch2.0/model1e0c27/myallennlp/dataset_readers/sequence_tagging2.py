8c15c0fc95dd5258813b15b15d6252f25f029c38
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 02:30:28 -0500

    model1e0c27/myallennlp/dataset_readers/sequence_tagging2.py - Update dfor batch processing of tags

--- train_model/articles_batch2.0/model1e0c27/myallennlp/dataset_readers/sequence_tagging2.py
+++ train_model/articles_batch2.0/model1e0c27/myallennlp/dataset_readers/sequence_tagging2.py
@@ -65,72 +65,52 @@ class SequenceTaggingDatasetReader(DatasetReader):
 
     @overrides
     def _read(self, file_path):
-        debug_mode = True
+        debug_mode = False
+        batch_process = True
+        import time
 
         # if `file_path` is a URL, redirect to the cache
         file_path = cached_path(file_path)
 
-        with open(file_path, "r") as data_file:
+        # Do full batch
+        if batch_process:
+            t0 = time.time()
+            with open(file_path, "r") as data_file:
 
-            logger.info("Reading instances from lines in file at: %s", file_path)
-            for line in data_file:
-                line = line.strip("\n")
+                logger.info("Reading instances from lines in file at: %s", file_path)
+                sentences_batch = []
+                tags_batch = []
+                for line in data_file:
+                    line = line.strip("\n")
+                    # skip blank lines
+                    if not line:
+                        continue
 
-                # skip blank lines
-                if not line:
-                    continue
+                    tokens_and_tags = [pair.rsplit(self._word_tag_delimiter, 1)
+                                       for pair in line.split(self._token_delimiter)]
 
+                    # davedit
+                    # Get text only
+                    words = [word for word, tag in tokens_and_tags]
+                    sentences_batch.append(' '.join(words))
 
-                tokens_and_tags = [pair.rsplit(self._word_tag_delimiter, 1)
-                                   for pair in line.split(self._token_delimiter)]
+                    tags_batch.append([tag for token, tag in tokens_and_tags])
 
-                # davedit
-                # Get text only
-                words = [word for word, tag in tokens_and_tags]
-                sentence = ' '.join(words)
-                # if debug_mode:
-                #     words2 = ws.split_words(sentence);
-                    # if not words == words2:
-                    #     import pdb;
-                    #     pdb.set_trace()
+            # Batch process tokens (faster)
+            tokens_batch = self._mytokenizer.batch_tokenize(sentences_batch)
 
-                # sentence = sentence.replace(' "— ','"—')
-                # sentence = sentence.replace('— ','—')
-                # sentence = sentence.replace(' \'','\'')          # Delete any spaces appearing before a '
-                                                                 # Necessary because spacy is stupid
-                             # For example, it splits I 'm into I, ', m  (e.g., len=3)
-                             # but splits I'm into I, 'm (e.g., len = 2)
+            t1 = time.time()
+            total = t1-t0
+            print("Token batch process time:")
+            print(total)
 
-
-                if self._use_spacy_directly:
-                    # # # # Usings spacy directly # # # #
-                    # Load into spacy
-                    doc = nlp(sentence)
-
-                    # Calculate pos
-                    spacy_pos = [token.pos_ for token in doc]
-
-                    # Calculate tags
-                    spacy_tags = [token.tag_ for token in doc]
-
-                    # Calculate NER tags
-                    pos_tags = [token.ent_type_ for token in doc]
-
-                    # Testing only
-                    # temp = [(tt[0],tt[1],a,b) for tt,a,b in zip(tokens_and_tags,spacy_pos,spacy_tags)]
-                    tokens = [Token(text=tt[0],pos=pos,tag=tag) for tt,pos,tag in zip(tokens_and_tags,spacy_pos,spacy_tags)]
-                else:
-                    # Use specified mytokenizer
-                    # Need to be sure to use modified white space tokenizer (registerd as whitespacy)
-                    # because the default option doesn't always reproduce the original tokenization
-                    # from versoin re-joined by spaces above.
-                    tokens = self._mytokenizer.tokenize(sentence)
-
-                #tokens = [Token(text=token) for token, tag in tokens_and_tags]
-                tags = [tag for token, tag in tokens_and_tags]
+            for i in range(len(tokens_batch)):
+                tokens = tokens_batch[i]
+                tags = tags_batch[i]
 
                 # Bug checking because spacy is stupid....grrrr
                 if debug_mode:
+                    words = sentences_batch[i].split()
                     words2 = [t.text for t in tokens]
                     if not words == words2:
                         import pdb
@@ -149,6 +129,79 @@ class SequenceTaggingDatasetReader(DatasetReader):
                     pdb.set_trace()
 
                 yield self.text_to_instance(tokens, tags)
+
+        if not batch_process:
+            t0 = time.time()
+            with open(file_path, "r") as data_file:
+
+                logger.info("Reading instances from lines in file at: %s", file_path)
+                for line in data_file:
+                    line = line.strip("\n")
+
+                    # skip blank lines
+                    if not line:
+                        continue
+
+
+                    tokens_and_tags = [pair.rsplit(self._word_tag_delimiter, 1)
+                                       for pair in line.split(self._token_delimiter)]
+
+                    # davedit
+                    # Get text only
+                    words = [word for word, tag in tokens_and_tags]
+                    sentence = ' '.join(words)
+
+                    if self._use_spacy_directly:
+                        # # # # Usings spacy directly # # # #
+                        # Load into spacy
+                        doc = nlp(sentence)
+
+                        # Calculate pos
+                        spacy_pos = [token.pos_ for token in doc]
+
+                        # Calculate tags
+                        spacy_tags = [token.tag_ for token in doc]
+
+                        # Calculate NER tags
+                        pos_tags = [token.ent_type_ for token in doc]
+
+                        # Testing only
+                        # temp = [(tt[0],tt[1],a,b) for tt,a,b in zip(tokens_and_tags,spacy_pos,spacy_tags)]
+                        tokens = [Token(text=tt[0],pos=pos,tag=tag) for tt,pos,tag in zip(tokens_and_tags,spacy_pos,spacy_tags)]
+                    else:
+                        # Use specified mytokenizer
+                        # Need to be sure to use modified white space tokenizer (registerd as whitespacy)
+                        # because the default option doesn't always reproduce the original tokenization
+                        # from versoin re-joined by spaces above.
+                        tokens = self._mytokenizer.tokenize(sentence)
+
+                    #tokens = [Token(text=token) for token, tag in tokens_and_tags]
+                    tags = [tag for token, tag in tokens_and_tags]
+
+                    # Bug checking because spacy is stupid....grrrr
+                    if debug_mode:
+                        words2 = [t.text for t in tokens]
+                        if not words == words2:
+                            import pdb
+                            pdb.set_trace()
+
+                    # If these don't match up, it will produce an error. In this csae,
+                    # pre-emptively enter debug mode
+                    if not len(tokens) == len(tags):
+                        # This is the orignal way tokens would have been calculated in sequence_tagging.py
+                        tokens2 = [Token(text=token) for token, tag in tokens_and_tags]
+                        print("Error -misamatch between tokens and tags")
+                        print(tokens)
+                        print(tokens2)
+
+                        import pdb
+                        pdb.set_trace()
+
+                    yield self.text_to_instance(tokens, tags)
+            t1 = time.time()
+            total = t1-t0
+            print("Iterated token processing time:")
+            print(total)
 
     def text_to_instance(self, tokens: List[Token], tags: List[str] = None) -> Instance:  # type: ignore
         """
