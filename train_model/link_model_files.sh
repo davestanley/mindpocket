60f3147b2655e3dee87655b570579c7475d811c3
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 18:33:33 -0500

    Updated link_model_files.sh and added links to steps_all.sh

--- train_model/link_model_files.sh
+++ train_model/link_model_files.sh
@@ -6,3 +6,4 @@ ln -s ../run_model_allarticles.py run_model_allarticles.py
 ln -s ../../step2_train_model.sh step2_train_model.sh
 ln -s ../../step3_pack_model.sh step3_pack_model.sh
 
+ln -s ../../steps_all.sh steps_all.sh
