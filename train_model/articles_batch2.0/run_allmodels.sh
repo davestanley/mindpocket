91cf58afbcb7689d179decf6af5ab4bfe72d966b
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 22:35:45 -0500

    Ran copy_script

--- train_model/articles_batch2.0/run_allmodels.sh
+++ train_model/articles_batch2.0/run_allmodels.sh
@@ -5,10 +5,7 @@ DIRECTORY=model_params
 for d in model*/ ; do
     echo "$d"
     cd $d
-    if [ ! -d "$DIRECTORY" ]; then
-       # If directory doesnt exist, run the script
-      echo ./steps_all.sh
-    fi
+    echo Running model in $d
+    ./steps_all.sh
     cd ..
 done
-
