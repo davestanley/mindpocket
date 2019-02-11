64f505fd93567fc0e06bcbaae3e6b0fbd7d7b304
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 21:36:42 -0500

    Updated copy_scripts and moved common scripts into scripts folder

--- train_model/articles_batch0.1/copy_scripts.sh
+++ train_model/articles_batch0.1/copy_scripts.sh
@@ -3,9 +3,10 @@
 for d in model*/ ; do
     echo "$d"
     cd $d
-    cp ../../steps_all.sh .
-    cp ../../step2_train_model.sh .
-    cp ../../step3_pack_model.sh .
+    cp ../../scripts/steps_all.sh .
+    cp ../../scripts/step2_train_model.sh .
+    cp ../../scripts/step3_pack_model.sh .
+    cp ../../scripts/test_performance_model.py .
     cp ../run_model_allarticles.py .
     #cp -r ../../myallennlp .
     cd ..
