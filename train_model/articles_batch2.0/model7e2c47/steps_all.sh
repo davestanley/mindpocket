6e4de46e47eb8fc766fc6f1a8ea1c08c6b8ccd5e
David Stanley <davearthur.stanley@gmail.com>
2019-02-11 01:52:22 -0500

    Updated steps_all

--- train_model/articles_batch2.0/model7e2c47/steps_all.sh
+++ train_model/articles_batch2.0/model7e2c47/steps_all.sh
@@ -11,6 +11,12 @@ if [ ! -d "$DIRECTORY" ]; then
   # If directory doesnt exist, run the script
   ./step2_train_model.sh
   ./step3_pack_model.sh
+  # Remove crap from training directory
+  rm model_params/model_state_epoch*.th
+  rm model_params/training_state_epoch*.th
+
+  # Force add a script in model_params, so the folder will exist, meaning we won't re-run it
+  git add -f model_params/stdout.log
 fi
 
 DIRECTORY=model_logs
