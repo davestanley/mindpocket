927bfbbd7c994fc3e89a078cb0fb0d1e97f8170b
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 21:38:05 -0500

    Ran copy_scripts

--- train_model/articles_batch2.0/model1e2c37/steps_all.sh
+++ train_model/articles_batch2.0/model1e2c37/steps_all.sh
@@ -4,5 +4,12 @@
 source activate allennlp
 ./step2_train_model.sh
 ./step3_pack_model.sh
+
+# Copy stdout logs over for safe keeping
+mkdir model_logs
+cp model_params/*.log model_logs/
+#git add -f model_logs/*.log
+
+# Finally, run the model on our data
 python run_model_allarticles.py
 
