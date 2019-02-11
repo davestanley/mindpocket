b88e32ab882c66e7de9096a3f11b857dabd3e1a5
David Stanley <davearthur.stanley@gmail.com>
2019-02-10 21:45:39 -0500

    Ran copy_script

--- train_model/articles_batch2.0/model0e0c27/steps_all.sh
+++ train_model/articles_batch2.0/model0e0c27/steps_all.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 # All steps necessary to run model
+
+# Activate allenNLP
 source activate allennlp
-./step2_train_model.sh
-./step3_pack_model.sh
 
-# Copy stdout logs over for safe keeping
-mkdir model_logs
-cp model_params/*.log model_logs/
-#git add -f model_logs/*.log
+# Train the model and pack the model too
+DIRECTORY=model_params
+if [ ! -d "$DIRECTORY" ]; then
+  # If directory doesnt exist, run the script
+  ./step2_train_model.sh
+  ./step3_pack_model.sh
+  mkdir model_logs
+  cp model_params/*.log model_logs/
+fi
 
-# Finally, run the model on our data
+# Run the model on our data
 python run_model_allarticles.py
 
+# Finally, test the model's performance
+python test_performance_model.py
